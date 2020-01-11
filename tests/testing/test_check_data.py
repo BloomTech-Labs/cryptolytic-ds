898d90b4fbde7d09a0ddda3d3123aa7da082a978
Ross Fisher <rcfisher1097@gmail.com>
2020-01-11 00:02:35 -0600

    live update can now retry for future data if there is a gap

--- tests/testing/test_check_data.py
+++ tests/testing/test_check_data.py
@@ -3,10 +3,11 @@ import cryptolytic.util.core as util
 import cryptolytic.data.historical as h
 
 def test_check_tables():
-    util.timeout(h.live_update, 20)
+    util.timeout(h.live_update, 10)
     for api, exchange_id, trading_pair in h.yield_unique_pair():
         df = (sql.get_some_candles
-                ({'api': api, 'exchange_id': exchange_id,'trading_pair': trading_pair},
+                ({'api': api, 'exchange_id': exchange_id, 'trading_pair': trading_pair,
+                  'period' : 300},
                  n=100000,
                  verbose=True))
         assert df.shape[0] > 100 # check to see that every trading pair has candles for it
