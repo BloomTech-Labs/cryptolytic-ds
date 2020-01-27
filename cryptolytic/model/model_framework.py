d709d5b7f37cd49d77f840cbbcba3514d62924b8
KyleHaggin <KyleHaggin@users.noreply.github.com>
2020-01-27 15:54:51 -0800

    added new model to model_framework.py

--- cryptolytic/model/model_framework.py
+++ cryptolytic/model/model_framework.py
@@ -1,17 +1,37 @@
 # begin imports
+from __future__ import print_function
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from __future__ import print_function
+import scipy
+import scipy.stats
+# from scipy.stats import yeojohnson
+import os
+from cryptolytic.util import *
 
 # tensorflow import
 import tensorflow as tf
 from tensorflow.keras.preprocessing import sequence
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Embedding
-from tensorflow.keras.layers import LSTM
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Dense, Embedding, Conv1D, Activation,\
+    Add, Input, LSTM
 import tensorflow.keras.layers as layers
+import tensorflow.keras.losses as losses
+import tensorflow.keras.models as models
+import tensorflow.keras.constraints as constraints
+import tensorflow.keras.regularizers as regularizers
+from tensorflow.keras.initializers import glorot_uniform, glorot_uniform
+
+# sklearn imports
+from sklearn.preprocessing import MinMaxScaler
+
+
+def thing(arg, axis=0):
+    x = np.sign(arg) * np.log(np.abs(arg) + 1)
+    mu = np.nanmean(x, axis=axis)
+    std = np.nanstd(x, axis=axis)
+    return x, mu, std
 
 
 def normalize_df(df):
@@ -25,6 +45,133 @@ def normalize_df(df):
 
 def denormalize_results(close_preds):
     return (1 + close_preds) * df.close[0]
+
+
+# normalization version 2
+def normalize(df):
+    if isinstance(df, pd.DataFrame) or isinstance(df, pd.Series):
+        df = df.values
+    if np.ndim(df) == 1:
+        df = np.expand_dims(df, axis=1)
+    df = df.copy()
+    x, mu, std = thing(df, axis=0)
+    for i in range(df.shape[1]):
+        df[:, i] = (x[:, i] - mu[i]) / stf[i]
+    return df
+
+
+# denormalization version 2
+def denormalize(values, df, col=None):
+    values = values.copy()
+
+    def eq(x, mu, std):
+        return np.exp((x*std) + mu) - 1
+
+    if np.ndim(values) == 1 and col is not None:
+        x, mu, std = thing(df[col])
+        return eq(values, mu, std)
+    else:
+        for i in range(values.shape[1]):
+            x, mu, std = thing(df.iloc[:, i])
+            if isinstance(values, pd.DataFrame):
+                values.iloc[:, i] = eq(values.iloc[:, i], mu, std)
+            else:
+                values[:, i] = eq(values[:, i], mu, std)
+    return values
+
+
+def windowed(df, target, batch_size, history_size, step, lahead=1, ratio=0.8):
+    xs = []
+    ys = []
+
+    x = dataset
+    y = dataset[:, target]
+
+    start = history_size # 1000
+    end = df.shape[0] - lahead # 4990
+    # 4990 - 1000 = 3990
+    for i in range(start, end):
+        # grab rows from i, to i+history_size
+        indices = range(i-history_size, i, step)
+        xs.append(x[indices])
+        ys.append(y[i:i+lahead])
+
+    xs = np.array(xs)
+    ys = np.array(ys)
+
+    nrows = xs.shape[0]
+    train_size = int(nrows * ratio)
+    # make sure the sizes are multiples of the batch size (needed for stateful lstm)
+    train_size -= train_size % batch_size
+    val_size = nrows - train_size
+    val_size -= val_size % batch_size
+    total_size = train_size + val_size
+    xs = xs[:total_size]
+    ys = ys[:total_size]
+
+    return xs[:train_size], ys[:train_size], xs[train_size:], ys[train_size:]
+
+
+def conv_block(X, filters, f, stage, block, strides=2):
+    """
+    X -- input tensor
+    f -- interger specify shape of conv window
+    stage - integer, used to named layers, depending on position in network.
+    stride -- stride
+    """
+    conv_base_name = 'conv' + str(stage) + block
+    bn_base_name = 'bn' + str(stage) + block
+    
+    f1, f2, f3 = filters
+    
+    X_shortcut = X
+    X = Conv1D(f1, kernel_size=1, strides=strides, name=conv_base_name + '_1', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = Activation('tanh')(X)
+    
+    X = Conv1D(f2, kernel_size=f, strides=1, padding='same', name=conv_base_name + '_2', 
+               kernel_initializer=glorot_uniform(seed=0))(X)
+    X = Activation('tanh')(X)
+    
+    X = Conv1D(f3, kernel_size=1, strides=1, padding='valid', name=conv_base_name + '_3', 
+               kernel_initializer=glorot_uniform(seed=0))(X)
+    # skip connection
+    X_shortcut = Conv1D(f3, kernel_size=1, strides=strides, padding='valid', name=conv_base_name + '_4', 
+               kernel_initializer=glorot_uniform(seed=0))(X_shortcut)
+    
+    assert tf.keras.backend.int_shape(X) == tf.keras.backend.int_shape(X_shortcut)
+    X = Add()([X, X_shortcut])
+    X = Activation('tanh')(X)
+
+    return X
+
+
+def identity_block(X, f, filters, stage, block, strides=2):
+    """
+    X -- input tensor
+    f -- interger specify shape of conv window
+    stage - integer, used to named layers, depending on position in network.
+    stride -- stride
+    """
+    conv_base_name = 'conv' + str(stage) + block
+    bn_base_name = 'bn' + str(stage) + block
+    
+    f1, f2, f3 = filters
+    
+    X_shortcut = X
+    X = Conv1D(f1, kernel_size=1, strides=strides, name=conv_base_name + '_1', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = Activation('tanh')(X)
+    
+    X = Conv1D(f2, kernel_size=f, strides=1, padding='same', name=conv_base_name + '_2', 
+               kernel_initializer=glorot_uniform(seed=0))(X)
+    X = Activation('tanh')(X)
+    
+    X = Conv1D(f3, kernel_size=1, strides=strides, padding='valid', name=conv_base_name + '_3', 
+               kernel_initializer=glorot_uniform(seed=0))(X)
+    # skip connection, without also convolving x_shortcut
+    assert tf.keras.backend.int_shape(X) == tf.keras.backend.int_shape(X_shortcut)
+    X = Add()([X, X_shortcut])
+    X = Activation('tanh')(X)
+    return X
 
 
 # should rename
@@ -125,17 +272,162 @@ def predictions():
     return preds
 
 
-def fit_model():
+# def fit_model():
 
-    model = lstm_model(x_train)
+#     model = lstm_model(x_train)
 
-    history = model.fit(train_data,
-                        epochs=9,
-                        steps_per_epoch=38,
-                        use_multiprocessing=True,
-                        workers=4,
-                        validation_data=val_data,
-                        validation_steps=5)  # (x_val, y_val))
+#     history = model.fit(train_data,
+#                         epochs=9,
+#                         steps_per_epoch=38,
+#                         use_multiprocessing=True,
+#                         workers=4,
+#                         validation_data=val_data,
+#                         validation_steps=5)  # (x_val, y_val))
+
+
+def fit_model(model, inputX, inputy):
+    epochs = 5
+    # batch size higher than 1 c  epochs = 10
+    for i in range(epochs):
+        model.fit(inputX,
+                  inputy, 
+                  batch_size=batch_size,
+                  epochs=1,
+                  verbose=1,
+                  use_multiprocessing=True,
+                  shuffle=False,
+                  workers=4,
+                  validation_data = (x_val, y_val))
+        history['loss'].append(model.history.history['loss'])
+        history['val_loss'].append(model.history.history['val_loss'])
+#        model.reset_states()
+    #pred = transformer.denormalize(model.predict(x_val)[:, 0], df, 'close')
+    #pred_history.append(pred)
+        
+    return model
+    
+def save_model(model, folder, params=None):
+    path = f'models/{folder}'
+    if not os.path.exists(path):
+        os.mkdir(path)
+    filename = os.path.join(path, 'model_' + str(np.random.rand()) + '.h5')
+    if os.path.exists(filename):
+        return save_model(model)
+    if params is not None:
+        param_path = os.path.join(path, 'model_params.csv')
+        pd.DataFrame(params).to_csv(param_path)
+        
+    model.save(filename)
+    print('Saved model')
+    
+def load_all_models(folder):
+    models = []
+    params = []
+    path = f'models/{folder}'
+    for m in os.listdir(path):
+        if m.endswith('.csv'):
+            params.append(pd.read_csv(m))
+        if not m.endswith('.h5'):
+            continue
+        m = tf.keras.models.load_model(m)
+        models.append(m)
+        print('Loaded %s' % path)
+    return models, params
+
+def stacked_dataset(models, inputX):
+    stackX = None
+    for model in models:
+        yhat = model.predict(inputX, verbose=0)
+        #stack predictions into [rows, members, probabilities]
+        if stackX is None:
+            stackX = yhat
+        else:
+            stackX = dstack((stackX, yhat))
+    # flatten predictions to [rows, members x probabilties]
+    stackX = stackX.reshape((stackX.shape[0], stackX.shape[1]*stackX.shape[2]))
+    return stackX
+
+def fit_stacked_model(models, inputX, inputy):
+    stackedX = stacked_dataset(models, inputX)
+    # fit model
+    model = create_model(inputX)
+    fit_model(model, inputX, inputy)
+    return model
+
+def stacked_prediction(models, model, inputX):
+    stackedX = stacked_dataset(models, inputX)
+    yhat = model.predict(stackedX)
+    return yhat
+
+def endingly():
+    models, params = load_all_models('example')
+    model = fit_stacked_model(models, x_train, y_train)
+    preds = stacked_prediction(models, model, x_train)
+    evaluate_models(models)
+    return preds
+
+def hyperparameter(inputX, inputy):
+    filtershape1 = 32 + 16 * np.random.randint(0, 6)
+    filtershape2 = 32 + 16 * np.random.randint(0, 6)
+    
+    params = adict(
+        filters1 = 32 + 16 * np.random.randint(0, 5),
+        noise1 = np.random.uniform(high=0.01),
+        filtershape1 = [filtershape1, filtershape1, filtershape1*2],
+        filtershape2 = [filtershape2, filtershape2, filtershape2*2]
+    )
+    print(params)
+    model = create_model(inputX, params)
+    fit_model(model, inputX, inputy)
+    save_model(model, 'filter', params=params)
+    return model
+                 
+def run_tuning():
+    models = []
+    for i in range(50):
+        models.append(hyperparameter(x_train, y_train))
+    return models
+
+   
+def evaluate_models(models):
+    for m in models:
+        _, acc = model.evaluate(x_val, y_val)
+
+
+def create_model(x_train, params):
+    attention_size = 5
+    input_shape = x_train.shape[-2:]
+    X_input = Input(input_shape, batch_size=batch_size)
+    X = X_input
+    X = layers.ZeroPadding1D(padding=2)(X)
+#    X = layers.TimeDistributed(Dense(input_shape[-1], kernel_constraint=constraints.max_norm(1.0), activation='tanh'))(X)
+    X = Conv1D(filters=params.filters1, kernel_size=6, strides=1, name='conv1', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = layers.GaussianNoise(params.noise1)(X)
+    #X = layers.AveragePooling1D(2, strides=1)(X)
+    X = conv_block(X, f=3, filters=params.filtershape1, stage=2, block='a', strides=2)
+    X = layers.GaussianNoise(params.noise1)(X)
+    X = identity_block(X, f=3, filters=params.filtershape1, stage=2, block='b', strides=1)
+    X = layers.GaussianNoise(params.noise1)(X)
+    X = identity_block(X, f=3, filters=params.filtershape1, stage=2, block='c', strides=1)
+    
+    print(X.get_shape())
+    print(X_input.get_shape())
+    #X = layers.concatenate([X, X_input])
+    
+   # X = layers.AveragePooling1D(2, name="avg_pool")(X)
+    
+#    rnn_cells = [tf.keras.layers.LSTMCell(128) for _ in range(2)]
+#    stacked_lstm = tf.keras.layers.StackedRNNCells(rnn_cells)
+#    lstm_layer = tf.keras.layers.RNN(stacked_lstm)
+#    X = lstm_layer(X)
+ 
+    X = layers.Flatten()(X)
+    X = Dense(256)(X)
+    X = Dense(lahead, kernel_initializer=glorot_uniform(seed=0))(X)
+    
+    model = Model(inputs=X_input, outputs=X, name='model1')
+    model.compile(loss='mse', optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.0001))
+    return model
 
 
 def create_framework_lstm(x_train, lstm_output_dimensionality=1):
@@ -150,7 +442,7 @@ def create_framework_lstm(x_train, lstm_output_dimensionality=1):
     # add lstm layers to the framework
     # input shape not yet added as import method not yet known
     framework_lstm.add(LSTM(lstm_output_dimensionality,
-                            input_shape=(x_train.shape[])))
+                            input_shape=(x_train.shape)))
     # add dense layers to the framework
     framework_lstm.add(Dense(1))
     # return the created framework
@@ -214,3 +506,32 @@ def model_fit(compiled_model,
                   validation_data=val_univariate,
                   validation_steps=validation_steps)
     return fit_model
+
+
+def multi_step_plot(history, true_future, prediction):
+    plt.figure(figsize=(12, 6))
+    num_in = create_time_steps(len(history))
+    num_out = len(true_future)
+    
+    plt.plot(num_in, np.array(history[:, 1]), label='History')
+    plt.plot(np.arange(num_out)/STEP, np.array(true_future), 'bo',
+        label='True Future')
+    if prediction.any():
+        plt.plot(np.arange(num_out)/STEP, np.array(prediction), 'ro',
+            label='Predicted Future')
+    plt.legend(loc='upper left')
+    plt.show()
+
+
+def plot_train_history(loss, val_loss, title):
+    epochs = range(len(loss))
+    
+    plt.figure()
+    
+    plt.plot(epochs, loss, 'b', label='Training loss')
+    plt.plot(epochs, val_loss, 'r', label='Validation loss')
+    plt.title(title)
+    plt.legend()
+    
+    plt.show()
+
