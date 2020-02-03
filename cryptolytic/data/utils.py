3e5266c89074a1f193d1a3643988a6e5be639df9
KyleHaggin <KyleHaggin@users.noreply.github.com>
2020-02-03 14:17:18 -0800

    cryptolytic.data.utils pep8 conformed

--- cryptolytic/data/utils.py
+++ cryptolytic/data/utils.py
@@ -1,12 +1,13 @@
 import psycopg2 as ps
 
 # Credentials
-credentials = {'POSTGRES_ADDRESS' : '#',
-               'POSTGRES_PORT' : '#',
-               'POSTGRES_USERNAME' : '#',
-               'POSTGRES_PASSWORD' : '#',
-               'POSTGRES_DBNAME' : '#',
-               'API_KEY' : '#'}
+credentials = {'POSTGRES_ADDRESS': '#',
+               'POSTGRES_PORT': '#',
+               'POSTGRES_USERNAME': '#',
+               'POSTGRES_PASSWORD': '#',
+               'POSTGRES_DBNAME': '#',
+               'API_KEY': '#'}
+
 
 # Define postgres_db_conn function.
 def postgres_db_conn(credentials):
@@ -27,6 +28,7 @@ def postgres_db_conn(credentials):
 # Define currency pairs within each exchange and create the names of the
 # tables for each exchange.
 
+
 coinbase_pro_pairs = ['bch_btc', 'bch_usd', 'btc_usd', 'btc_usdc', 'dash_btc',
                       'dash_usd', 'eos_btc', 'eos_usd', 'etc_usd', 'eth_btc',
                       'eth_usd', 'eth_usdc', 'ltc_btc', 'ltc_usd', 'xrp_btc',
@@ -37,7 +39,7 @@ bitfinex_pairs = ['bch_btc', 'bch_usd', 'bch_usdt', 'btc_usd', 'btc_usdt',
                   'etc_usd', 'eth_btc', 'eth_usd', 'eth_usdt', 'ltc_btc',
                   'ltc_usd', 'ltc_usdt', 'xrp_btc', 'xrp_usd', 'zec_usd',
                   'zrx_usd']
-                  
+
 hitbtc_pairs = ['bch_btc', 'bch_usdt', 'btc_usdc', 'btc_usdt', 'dash_btc',
                 'dash_usdt', 'eos_btc', 'eos_usdt', 'etc_usdt', 'eth_btc',
                 'eth_usdc', 'eth_usdt', 'ltc_btc', 'ltc_usdt', 'xrp_btc',
@@ -54,12 +56,13 @@ hitbtc_table_list = ['hitbtc_' + pair for pair in hitbtc_pairs]
 
 bitfinex_table_list = ['bitfinex_' + pair for pair in bitfinex_pairs]
 
-coinbase_pro_table_list = ['coinbase_pro_' + pair for pair in 
+coinbase_pro_table_list = ['coinbase_pro_' + pair for pair in
                            coinbase_pro_pairs]
 
 gemini_table_list = ['gemini_' + pair for pair in gemini_pairs]
 
 kraken_table_list = ['kraken_' + pair for pair in kraken_pairs]
+
 
 # Define create_tables function.
 def create_tables(credentials):
@@ -88,22 +91,26 @@ def create_tables(credentials):
             close float,
             base_volume float
             );'''.format(schema=schema, table_name=table_name))
-            
+
     # Commit and close. Verify that tables were created successfully.
     conn.commit()
-    
+
     print("Tables created successfully!")
     conn.close()
- 
+
+
 # Define insert_csv_to_db function.
-# Within Jupyter Lab, a folder entitled "data" was created. A folder for each exchange was
-# then created within the "data" folder. Each exchange folder then included csv files for all 
-# of the trading pairs supported on that respective exchange and for all intervals (300 and 3600).
+# Within Jupyter Lab, a folder entitled "data" was created.
+# A folder for each exchange wasthen created within the "data" folder.
+# Each exchange folder then included csv files for all of the trading pairs
+# supported on that respective exchange and for all intervals (300 and 3600).
 def insert_csv_to_db(credentials):
-  '''Connects to a PostgreSQL database and imports csv files into a specified schema
-  and table name.'''
-    
-    # Create connection and cursor to database.
+    '''
+    Connects to a PostgreSQL database and imports csv
+    files into a specified schema and table name.
+    '''
+
+# Create connection and cursor to database.
     conn, cur = create_conn(credentials)
 
     print("Connect to database.")
@@ -119,36 +126,41 @@ def insert_csv_to_db(credentials):
                     table_name = filename.replace('_3600.csv', '')
                 with open('data/' + directory + '/' + filename, 'r') as f:
                     # Skip the header row.
-                    next(f) 
-                    cur.copy_from(f, '{schema}.{table_name}'.format(schema=schema, table_name=table_name), sep=',')
+                    next(f)
+                    cur.copy_from(f, '{schema}.{table_name}'.format(
+                        schema=schema, table_name=table_name), sep=','
+                        )
                     conn.commit()
 
     conn.close()
     print('Done!')
 
+
 # Define drop_column function.
 def drop_column(credentials):
-  '''Connects to a PostgreSQL database and drops a table column, in this case ID,
-  from all tables within each schema.'''
-    
+    '''
+    Connects to a PostgreSQL database and drops a table column,
+    in this case ID, from all tables within each schema.
+    '''
+
     # Create connection and cursor to database.
     conn, cur = create_conn(credentials)
-    
+
     print("Connect to database.")
-    
+
     schemas = ['fiveminute', 'onehour']
-    table_list = (hitbtc_table_list + bitfinex_table_list + coinbase_pro_table_list + gemini_table_list + kraken_table_list)
-    
+    table_list = (hitbtc_table_list + bitfinex_table_list +
+                  coinbase_pro_table_list + gemini_table_list +
+                  kraken_table_list)
+
     for schema in schemas:
         for table_name in table_list:
-            cur.execute('''ALTER TABLE {schema}.{table_name} DROP COLUMN ID;'''.format(schema=schema, table_name=table_name))
-    
+            cur.execute(
+                '''ALTER TABLE {schema}.{table_name} DROP COLUMN ID;'''
+                .format(schema=schema, table_name=table_name))
+
     # Commit and close. Verify that tables were created successfully.
     conn.commit()
-    
+
     print("Column removed.")
     conn.close()
-
-
-# Utilities Involing dates
-def datetime
